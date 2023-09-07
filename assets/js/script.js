2603f4fb2e1dc2fab4d5ecca32cc4516937e00d0
galvin-SH <mgalvin1993@gmail.com>
2023-09-07 10:55:22 -0700

    set up type object

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,32 @@
 
 // DEBUG: forces functions to use the pokemon name defined by pokeSpecies
 const pokeSpecies = "pikachu";
+// Object holding all pokemon types with keys
+let types = {
+    1: "Normal",
+    2: "Fighting",
+    3: "Flying",
+    4: "Poison",
+    5: "Ground",
+    6: "Rock",
+    7: "Bug",
+    8: "Ghost",
+    9: "Steel",
+    10: "Fire",
+    11: "Water",
+    12: "Grass",
+    13: "Electric",
+    14: "Psychic",
+    15: "Ice",
+    16: "Dragon",
+    17: "Dark",
+    18: "Fairy"
+}
+
+function getTypeIcons(type) {
+
+}
+
 
 // Function that retrieves pokemon information and creates elements to hold the contents
 function getPokeInfo(pokeName) {
