c7c3808e965599b9b40bfca35910a1f345aff360
galvin-SH <mgalvin1993@gmail.com>
2023-09-07 02:38:32 -0700

    api calls to pokeapi and bulbapedia done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,71 @@
-// TODO: API get object with specific pokemon info (type(s), short description, etc) from pokeapi
+// TODO: Use api to retrieve pokemon type icons
+// TODO: Build object to hold type matchup data for the battle gamemode
+// TODO: Function to determine if the user selected the correct choice in the battle gamemode
 
-// TODO: API get object with pokemon image from bulbapedia
+// DEBUG: forces functions to use the pokemon name defined by pokeSpecies
+const pokeSpecies = "pikachu";
 
-// TODO: API get object with type icons from bulbapedia
-
-const pokeSpecies = "ivysaur";
-const dex = document.querySelector("#dex");
-
-
-
-function getPokeInfo(pokename) {
-    let apiURL = `https://pokeapi.co/api/v2/pokemon/${pokename}`
-    fetch(apiURL).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
+// Function that retrieves pokemon information and creates elements to hold the contents
+function getPokeInfo(pokeName) {
+    // Request url includes a pokemon name that is passed into the function (POKEAPI REQUIRES LOWERCASE POKEMON NAMES)
+    let apiURL = `https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}`
+    
+    // Sends request to pokeapi
+    fetch(apiURL)
+        .then(function (response) {
+            // Formats the response into a JSON object
+            return response.json();
+        })
+        .then(function (data) {
+            // Dynamically creates each of the elements to display a pokemon's info
             let pokeImg = document.createElement("img");
             let pokeName = document.createElement("h2");
             let pokeType1 = document.createElement("h3");
             let pokeType2 = document.createElement("h3");
             let pokeID = document.createElement("h3");
 
-            pokeImg.src = getPokeImage(pokeSpecies);
-            pokeName.textContent = "Name: " + data.name;
-            pokeType1.textContent = "Type 1: " + data.types[0].type.name;
+            // Sets the contents of each element according to JSON data
+            getPokeImage(pokeSpecies, pokeImg); // retrieves the bulbapedia img corresponding to the pokemon and assigns it as the src of pokeImg
+            pokeName.textContent = "Name: " + data.name.toUpperCase().slice(0, 1) + data.name.slice(1);
+            pokeType1.textContent = "Type 1: " + data.types[0].type.name.toUpperCase().slice(0, 1) + data.types[0].type.name.slice(1);
             if (data.types.length == 2) {
                 pokeType2.textContent = "Type 2: " + data.types[1].type.name;
             }
-            else { pokeType2.textcontent = "N/A" }
+            // Sets contents to 'N/A' for the pokeType2 element if the pokemon only has one type
+            else { pokeType2.textContent = "Type 2: N/A" }
             pokeID.textContent = "ID Number: " + data.id;
-
-            dex.append(pokeImg, pokeName, pokeType1, pokeType2, pokeID);
+            // Appends the created elements onto the body of the document
+            document.body.append(pokeImg, pokeName, pokeType1, pokeType2, pokeID);
         })
-    })
 }
 
-function getPokeImage(pokename) {
-    let apiURL = "https://bulbapedia.bulbagarden.net/w/api.php";
+// This function retrieves a pokemon image from bulbapedia and assigns it as the source of the image element provided to the function
+function getPokeImage(pokeName, targetEl) {
+    // Base url to build request url from
+    let apiURL = "https://bulbapedia.bulbagarden.net/w/api.php?origin=*"; 
+    // Object that holds the keys and parameters to build the request url from
     let params = {
         action: "query",
         format: "json",
-        prop: "images",
-        titles: `${pokename}_(Pokémon)`
+        prop: "pageimages",
+        indexpageids: 1,
+        piprop: "original",
+        titles: `${pokeName}_(Pokémon)`
     }
-    apiURL = apiURL + "?origin=*";
-
+    // Builds the request url from the keys and parameters provided
     Object.keys(params).forEach(function (key) { apiURL += "&" + key + "=" + params[key]; });
     
-    fetch(apiURL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (response){
-        let imgURL = Object.values(response.query.pages)[0].images[2].title
-        console.log(imgURL);
-    })
-    .catch(function (error) {console.log(error); });
+    // Sends request to bulbapedia via mediawiki api
+        fetch(apiURL)
+        .then(function (response) {
+            // Formats the response into a JSON object
+            return response.json();
+        })
+        .then(function (data) {
+            let pid = data.query.pageids[0];
+            targetEl.src = data.query.pages[pid].original.source;
+        })
+        .catch(function (error) { console.log(error); });
 }
 
-
-//Example Function From https://github.com/wikimedia/mediawiki-api-demos
-// var url = "https://en.wikipedia.org/w/api.php";
-
-// var params = {
-//     action: "query",
-//     prop: "images",
-//     titles: "Albert Einstein",
-//     format: "json"
-// };
-
-// url = url + "?origin=*";
-// Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
-
-// fetch(url)
-//     .then(function (response) { return response.json(); })
-//     .then(function (response) {
-//         var pages = response.query.pages;
-//         for (var page in pages) {
-//             for (var img of pages[page].images) {
-//                 console.log(img.title);
-//             }
-//         }
-//     })
-//     .catch(function (error) { console.log(error); });
-
-getPokeInfo(pokeSpecies);
-// getPokeImage(pokeSpecies);
+getPokeInfo(pokeSpecies); // Requests api information for the pokemon name passed in to the function
