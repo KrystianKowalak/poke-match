289548ca0d6d6814e914443732b2617cd33f1812
Cossack <kkowalak9@gmail.com>
2023-09-09 12:42:34 -0700

    Fixed async functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
-// TODO: Use api to retrieve pokemon type icons
-// TODO: Build object to hold type matchup data for the battle gamemode
-// TODO: Function to determine if the user selected the correct choice in the battle gamemode
-
 //{"id": 0, "name": "null", "type1": "null", "type2" : "null", "flavor_text": "null" "habitat": "null", "stats": {}, "abilities": {}, "sprites": {}}
 let pokeData = [];
+
 let pokemonByTypes = [];
 
 String.prototype.replaceAt = function(index, replacement) {
@@ -12,8 +9,8 @@ String.prototype.replaceAt = function(index, replacement) {
 
 //------------------------------API CALLS------------------------------
 //----------POKEAPI----------
-function fetchAllData() {
-    fetch("https://pokeapi.co/api/v2/pokemon-species/?offset=0&limit=110")
+function fetchNameIDData() {
+    return fetch("https://pokeapi.co/api/v2/pokemon-species/?offset=0&limit=110")
         .then(function (response) {
             if (!(response.status == 200)) {
 
@@ -22,63 +19,71 @@ function fetchAllData() {
             return response.json();
         })
         .then(function (data) {
-            for (let i = 0; i < data.count; i++) {
-                const dataObject = {"id": 0, "name": "null", "type1": "null", "type2" : "null", "flavor_text": "null", "habitat": "null", "stats": {}, "abilities": {}, "sprites": {}};
+            for (let i = 0; i < 110; i++) {
+                let dataObject = {"id": 0, "name": "null", "type1": "null", "type2" : "null", "flavor_text": "null", "habitat": "null", "stats": {}, "abilities": {}, "sprites": {}};
                 dataObject.id = i + 1;
                 dataObject.name = data.results[i].name;
                 pokeData.push(dataObject);
-
-                fetch("https://pokeapi.co/api/v2/pokemon/" + (i + 1))
-                    .then(function (response) {
-                        if (!(response.status == 200)) {
-    
-                            throw new Error("Not 2xx response", {cause: response});
-                        }
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        pokeData[i].type1 = data.types[0].type.name.toUpperCase().slice(0, 1) + data.types[0].type.name.slice(1);
-                        if (data.types.length == 2) {
-                            pokeData[i].type2 = data.types[1].type.name.toUpperCase().slice(0, 1) + data.types[1].type.name.slice(1);
-                        }
-                        else {
-                            pokeData[i].type2 = "N/A"
-                        }
-                        pokeData[i].stats = data.stats;
-                        pokeData[i].abilities = data.abilities;
-                        pokeData[i].sprites = data.sprites;
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-                
-                fetch("https://pokeapi.co/api/v2/pokemon-species/" + (i + 1))
-                    .then(function (response) {
-                        if (!(response.status == 200)) {
-    
-                            throw new Error("Not 2xx response", {cause: response});
-                        }
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        pokeData[i].habitat = data.habitat.name;
-                        for (let index = 0; index < data.flavor_text_entries.length; index++) {
-                            if(data.flavor_text_entries[index].language.name == "en") {
-                                pokeData[i].flavor_text = data.flavor_text_entries[index].flavor_text
-                                pokeData[i].flavor_text = pokeData[i].flavor_text.replaceAt(pokeData[i].flavor_text.search("é"), "e")
-                                break;
-                            }
-                        }
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
            }
         })
         .catch(function (error) {
             console.log(error);
         });
-    console.log(pokeData);
+}
+
+function fetchInfoData() {
+    for (let i = 0; i < 110; i++) {
+        return fetch("https://pokeapi.co/api/v2/pokemon/" + (i + 1))
+            .then(function (response) {
+                if (!(response.status == 200)) {
+        
+                    throw new Error("Not 2xx response", {cause: response});
+                }
+                    return response.json();
+            })
+            .then(function (data) {
+                pokeData[i].type1 = data.types[0].type.name.toUpperCase().slice(0, 1) + data.types[0].type.name.slice(1);
+                if (data.types.length == 2) {
+                    pokeData[i].type2 = data.types[1].type.name.toUpperCase().slice(0, 1) + data.types[1].type.name.slice(1);
+                }
+                    else {
+                    pokeData[i].type2 = "N/A"
+                }
+                pokeData[i].stats = data.stats;
+                pokeData[i].abilities = data.abilities;
+                pokeData[i].sprites = data.sprites;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
+function fetchInfoData2() {
+    for (let i = 0; i < 110; i++) {
+        return fetch("https://pokeapi.co/api/v2/pokemon-species/" + (i + 1))
+            .then(function (response) {
+                if (!(response.status == 200)) {
+    
+                    throw new Error("Not 2xx response", {cause: response});
+                }
+                return response.json();
+            })
+                .then(function (data) {
+                pokeData[i].habitat = data.habitat.name;
+                for (let index = 0; index < data.flavor_text_entries.length; index++) {
+                    if(data.flavor_text_entries[index].language.name == "en") {
+                        pokeData[i].flavor_text = data.flavor_text_entries[index].flavor_text
+                        pokeData[i].flavor_text = pokeData[i].flavor_text.replaceAt(pokeData[i].flavor_text.search("é"), "e")
+                        pokeData[i].flavor_text = pokeData[i].flavor_text.replaceAt(pokeData[i].flavor_text.search("\f"), "")
+                        break;
+                    }
+                }
+             })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 }
 //----------MEDIAWIKIAPI----------
 // This function retrieves a pokemon image from bulbapedia and assigns it as the source of the image element provided to the function
@@ -92,7 +97,7 @@ function getPokeImage(pokeName, targetEl) {
         prop: "pageimages",
         indexpageids: 1,
         piprop: "original",
-        titles: `${pokeName}_(Pokémon)`
+        titles: `${pokeName.toLowerCase()}_(Pokémon)`
     }
     // Builds the request url from the keys and parameters provided
     Object.keys(params).forEach(function (key) { apiURL += "&" + key + "=" + params[key]; });
@@ -119,6 +124,28 @@ function getPokeImage(pokeName, targetEl) {
         });
 }
 //------------------------------ICON BUTTONS------------------------------
+// Object holding all pokemon types and their background colors
+const types = {
+    "normal": "#A8A77A",
+    "fighting": "#C22E28",
+    "flying": "#A98FF3",
+    "poison": "#A33EA1",
+    "ground": "#E2BF65",
+    "rock": "#B6A136",
+    "bug": "#A6B91A",
+    "ghost": "#735797",
+    "steel": "#B7B7CE",
+    "fire": "#EE8130",
+    "water": "#6390F0",
+    "grass": "#7AC74C",
+    "electric": "#F7D02C",
+    "psychic": "#F95587",
+    "ice": "#96D9D6",
+    "dragon": "#6F35FC",
+    "dark": "#705746",
+    "fairy": "#D685AD"
+};
+
 function assignIcons(types) {
     let iconContainer = document.createElement("div");
     for (i = 0; i < types.length; i++) {
@@ -171,90 +198,49 @@ function getTypeIcon(type, targetEl) {
         });
 }
 
-// Object holding all pokemon types and their background colors
-const types = {
-    "normal": "#A8A77A",
-    "fighting": "#C22E28",
-    "flying": "#A98FF3",
-    "poison": "#A33EA1",
-    "ground": "#E2BF65",
-    "rock": "#B6A136",
-    "bug": "#A6B91A",
-    "ghost": "#735797",
-    "steel": "#B7B7CE",
-    "fire": "#EE8130",
-    "water": "#6390F0",
-    "grass": "#7AC74C",
-    "electric": "#F7D02C",
-    "psychic": "#F95587",
-    "ice": "#96D9D6",
-    "dragon": "#6F35FC",
-    "dark": "#705746",
-    "fairy": "#D685AD"
-};
-
-function typeButtons(types, location) {
-    let buttonContainer = document.createElement("div");
-    let typesArray = Object.keys(types);
-    let hexArray = Object.values(types);
-    // buttonContainer.style.setProperty("display", "flex");
-    // buttonContainer.style.setProperty("width", "250px");
-    // buttonContainer.style.setProperty("flex-wrap", "wrap");
-    // buttonContainer.style.setProperty("justify-content", "center");
-    // buttonContainer.style.setProperty("border-style", "solid");
-    // buttonContainer.style.setProperty("border-width", "3px");
-    // buttonContainer.style.setProperty("padding", "10px");
-    // buttonContainer.style.setProperty("gap", "10px");
-
-    for (i = 0; i < typesArray.length; i++) {
-        let typeButton = document.createElement("button");
-        typeButton.textContent = typesArray[i];
-        typeButton.id = typesArray[i] + "-button";
-        // typeButton.style.setProperty("background-color", hexArray[i]);
-        // typeButton.style.setProperty("color", "white");
-        // typeButton.style.setProperty("min-width","66px")
-        // typeButton.style.setProperty("border-radius","5px")
-        typeButton.onclick = function() {
-            console.log(typeButton.textContent);
-        };
-        // buttonContainer.append(typeButton);
-    }
-    location.append(buttonContainer);
-}
-
 // work in progress
 function setPokedexInfo() {
-    typeButtons(types, document.querySelector(".types"));
-    getPokeImage(pokeSpecies, document.getElementById("pokedex-image"));
-    getPokeInfo(pokeSpecies, document.getElementById("Pokemon-name"), document.getElementById("Pokemon-type1"), document.getElementById("Pokemon-type2"), document.getElementById("Pokemon-id"));
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeSpecies.toLowerCase()}`)
-        .then(function (response) {
-            if (!(response.status == 200)) {
-
-                throw new Error("Not 2xx response", {cause: response});
-            }
-            return response.json();
-        })
-        .then(function (data) {
-            getPokemonFlavor_text(data.id, document.getElementById("flavor-text"));
-            document.querySelector(".facts").append(document.getElementById(`${data.types[0].type.name}-button`))
-            if (data.types.length == 2) {
-                document.querySelector(".facts").append(document.getElementById(`${data.types[1].type.name}-button`))
-            }
-            document.getElementById("pokemon-link").href = `https://bulbapedia.bulbagarden.net/wiki/${data.name}_(Pok%C3%A9mon)`;
-        document.getElementById("pokemon-link").textContent = `Bulbapedia-${data.name}`;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    getPokeImage(pokeData[0].name, document.getElementById("pokedex-image"));
+    assignIcons(Object.keys(types));
+    console.log(pokeData[0].name);
+    document.getElementById("flavor-text").textContent = pokeData[0].flavor_text;
 }
 
 //------------------------------Executable Code------------------------------
-
 // Runs on site load to load functions
-document.addEventListener('DOMContentLoaded', function() {
-    fetchAllData()
-    //setPokedexInfo();
-    assignIcons(Object.keys(types));
+document.addEventListener('DOMContentLoaded', async function() {
+    await fetchNameIDData();
+    await fetchInfoData();
+    await fetchInfoData2();
+    console.log(pokeData);
+    setPokedexInfo();
 });
+
+// function typeButtons(types, location) {
+//     let buttonContainer = document.createElement("div");
+//     let typesArray = Object.keys(types);
+//     let hexArray = Object.values(types);
+//     // buttonContainer.style.setProperty("display", "flex");
+//     // buttonContainer.style.setProperty("width", "250px");
+//     // buttonContainer.style.setProperty("flex-wrap", "wrap");
+//     // buttonContainer.style.setProperty("justify-content", "center");
+//     // buttonContainer.style.setProperty("border-style", "solid");
+//     // buttonContainer.style.setProperty("border-width", "3px");
+//     // buttonContainer.style.setProperty("padding", "10px");
+//     // buttonContainer.style.setProperty("gap", "10px");
+
+//     for (i = 0; i < typesArray.length; i++) {
+//         let typeButton = document.createElement("button");
+//         typeButton.textContent = typesArray[i];
+//         typeButton.id = typesArray[i] + "-button";
+//         // typeButton.style.setProperty("background-color", hexArray[i]);
+//         // typeButton.style.setProperty("color", "white");
+//         // typeButton.style.setProperty("min-width","66px")
+//         // typeButton.style.setProperty("border-radius","5px")
+//         typeButton.onclick = function() {
+//             console.log(typeButton.textContent);
+//         };
+//         // buttonContainer.append(typeButton);
+//     }
+//     location.append(buttonContainer);
+// }
